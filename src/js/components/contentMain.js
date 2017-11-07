76b22fc7b1716c59c5db135b5e1e150c808090bc
justyouhappy <593296610@qq.com>
2017-11-07 15:28:16 +0800

    增加弹窗组件

--- src/js/components/contentMain.js
+++ src/js/components/contentMain.js
@@ -2,20 +2,36 @@ import React from 'react';
 import '../../scss/contentMain.scss';
 import InformationList from './informationList';
 import SignIn from './signIn';
+import ConfirmModel from './confirmModal';
 
 class ContentMain extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
+			visible: true
 		}
+		this.onHidden = this.onHidden.bind(this);
+		this.onShow = this.onShow.bind(this);
+	}
+	onHidden() {
+		this.setState({
+			visible: false
+		})
+	}
+	onShow() {
+		this.setState({
+			visible: true
+		})
 	}
 	render() {
-        const { setInformation, changeId } = this.props;
+		const { setInformation, changeId } = this.props;
+		const { visible } = this.state;
         return(
             <div className="contnet-main-body">
 				<div className="first-row">
 					<InformationList className="information-list" setInformation={setInformation} changeId={changeId}/>
-					<SignIn />	
+					<SignIn onHidden={this.onHidden} onShow={this.onShow}/>
+					{visible && <ConfirmModel onHidden={this.onHidden}/>}
 				</div>
             </div>
         )
