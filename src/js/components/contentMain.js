1c8fd817ec545e347399662525f14a241905ab6b
justyouhappy <593296610@qq.com>
2017-11-07 14:03:42 +0800

    新增登录组件&更改contentMain布局

--- src/js/components/contentMain.js
+++ src/js/components/contentMain.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../../scss/contentMain.scss';
 import InformationList from './informationList';
-
+import SignIn from './signIn';
 
 class ContentMain extends React.Component {
 	constructor(props) {
@@ -13,7 +13,10 @@ class ContentMain extends React.Component {
         const { setInformation, changeId } = this.props;
         return(
             <div className="contnet-main-body">
-                <InformationList className="information-list" setInformation={setInformation} changeId={changeId}/>
+				<div className="first-row">
+					<InformationList className="information-list" setInformation={setInformation} changeId={changeId}/>
+					<SignIn />	
+				</div>
             </div>
         )
 
