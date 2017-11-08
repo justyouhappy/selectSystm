b747458947c3bf87a50b61288f0e92416fea9562
hejiale <2352851038@qq.com>
2017-11-08 22:50:17 +0800

    登陆修改11.8

--- src/js/components/signIn.js
+++ src/js/components/signIn.js
@@ -2,7 +2,8 @@ import React from 'react';
 import '../../scss/signIn.scss';
 import { 
 	Button,
-	Input
+	Input,
+	Alert
  } from 'antd';
 
 class SignIn extends React.Component {
@@ -48,7 +49,23 @@ class SignIn extends React.Component {
 		})
 	}
 	signIn() {
-		console.log(this.state);
+		let signInAlert1 = document.getElementsByClassName("signInAlert1")[0],
+			signInAlert2 = document.getElementsByClassName("signInAlert2")[0],
+			signInAlert3 = document.getElementsByClassName("signInAlert3")[0];               
+		if(this.state.userName == '') { 
+			    signInAlert1.style.display = "block";
+		}else{
+			if(this.state.tel == '' && this.state.staus == 0) {
+				signInAlert2.style.display = "block";
+			} else{
+					if(this.state.password == '') {
+						signInAlert3.style.display = "block";
+					}
+					else{
+						console.log(this.state);
+				  }
+			}
+		}
 	}
 	render() {
 		const { staus, userName, tel, password } = this.state;
@@ -60,6 +77,9 @@ class SignIn extends React.Component {
                 </div>
                 <div className="sign-in-body-under">
                     <div className="btn-box">
+					    <Alert message="请您输入学号/工号" type="error" showIcon className="signInAlert1" />
+						<Alert message="请您输入电话号码" type="error" showIcon className="signInAlert2" />
+						<Alert message="请您输入密码" type="error" showIcon className="signInAlert3" />
                         <Input size="large" onChange={this.userChange} placeholder={staus === "0" ? "学号/工号": '管理员账号'} value={userName} className="signinput"/>
                         {staus === "0" && <Input size="large" placeholder="电话号码" value={tel} onChange={this.telChange} className="signinput" />}
                         <Input size="large" placeholder="密码" value={password} onChange={this.passwordChange} type="password" className="signinput"/>
