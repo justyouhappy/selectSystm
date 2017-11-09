33b5ebc936888ec1d64a6aaa2c5b6890fb70018d
justyouhappy <593296610@qq.com>
2017-11-09 12:45:02 +0800

    恢复配置

--- webpack.config.js
+++ webpack.config.js
@@ -65,7 +65,7 @@ module.exports = {
 		]
 	},
 	devServer: {
-		port: 8082,
+		port: 8080,
 		historyApiFallback: true,
 		inline: true, //注意：不写hot: true，否则浏览器无法自动更新；也不要写colors:true，progress:true等，webpack2.x已不支持这些
 	},
