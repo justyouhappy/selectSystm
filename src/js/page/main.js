4e08cc1e3ab6386f595d67420513c5dad89e6920
justyouhappy <593296610@qq.com>
2017-11-14 00:10:31 +0800

    登录成功页布局

--- src/js/page/main.js
+++ src/js/page/main.js
@@ -2,16 +2,12 @@ import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as actions from '../redux/action/mainAction';
-import fetchdata from '../common/fetch';
 import TopImg from '../containers/topImg';
 import Content from '../containers/content';
 import '../../scss/main.scss';
 import {
     Button
 } from 'antd';
-// fetchdata(//地址字符串).then(data => {
-//    data 获取的数据
-// });
 class Main extends React.Component {
 	constructor(props) {
 		super(props);
--- src/js/page/success.js
+++ src/js/page/success.js
@@ -4,8 +4,11 @@ import { connect } from 'react-redux';
 import fetchdata from '../common/fetch';
 import TopImg from '../containers/topImg';
 import {
-    Button
+    Button,
+    Tabs
 } from 'antd';
+import '../../scss/success.scss';
+const TabPane = Tabs.TabPane;
 // fetchdata(//地址字符串).then(data => {
 //    data 获取的数据
 // });
@@ -20,6 +23,32 @@ class Success extends React.Component {
 		return (
             <div className="bodyer">  
 				<TopImg num={window.num}/>
+                <Tabs type="card">
+                    <TabPane tab="我的档案" key="1">
+						<div className="success-content">Content of Tab Pane 1</div>
+					</TabPane>
+					<TabPane tab="档案查询" key="5">
+						<div className="success-content">Content of Tab Pane 2</div>
+					</TabPane>
+					<TabPane tab="资料下载" key="2">
+						<div className="success-content">Content of Tab Pane 1</div>
+					</TabPane>
+					<TabPane tab="档案/资料上传" key="3">
+						<div className="success-content">Content of Tab Pane 1</div>
+					</TabPane>
+					<TabPane tab="审核" key="4">
+						<div className="success-content">Content of Tab Pane 1</div>
+					</TabPane>
+					<TabPane tab="发布公告" key="8">
+						<div className="success-content">Content of Tab Pane 1</div>
+					</TabPane>
+                    <TabPane tab="修改密码" key="6">
+						<div className="success-content">Content of Tab Pane 3</div>						
+					</TabPane>
+					<TabPane tab="退出" key="7">
+						<div className="success-content">Content of Tab Pane 3</div>						
+					</TabPane>
+                </Tabs>
             </div>
 		);
 	}
