3e316f461220ef02b2adee9bf4f541c89e3ad6da
justyouhappy <593296610@qq.com>
2017-11-15 12:14:01 +0800

    新增上传组件

---
+++ src/js/components/upload.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../../scss/upload.scss';
+import {
+} from 'antd';
+class Upload extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+		}
+	}
+	render() {
+		return (
+            <div>  
+				
+            </div>
+		);
+	}
+}
+
+export default Upload;
